ebdba04b3abfe24ae161b7bed6073a349f8bb76a
VitliiSheremet <shervitt1982@gmail.com>
2024-05-11 20:42:10 +0300

    add pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,32 @@ import { getAllArticles } from './(server)/api';
 import { ROUTING } from './routing';
 import { AppLink } from './shared/components/app-link';
 
-export default async function Home() {
-  const allArticles = await getAllArticles()
-  return <>
+const ARTICLES_PER_PAGE = 10;
+
+export default async function Home({
+  searchParams,
+} : {
+  searchParams: Record<string, string>;
+}) {
+  console.log(searchParams['page'])
+  const page = Number.parseInt(searchParams['page'] ?? 1)
+  const allArticles = await getAllArticles();
+  const articles = allArticles.slice(
+    (page-1) * ARTICLES_PER_PAGE,
+    page * ARTICLES_PER_PAGE
+);
+
+const nextPageUrl ={
+  search: new URLSearchParams({
+    page: (page + 1).toString()
+   }).toString(),
+}
+
+  return (
+  <>
   <ul>
-  <h1>Drag13 blog</h1>
-    {allArticles.map((article)=> (
+  <h1>Drag13 blog, page {page} </h1>
+    {articles.map((article)=> (
       <li key={article.name}>
         <AppLink href={ROUTING.article(article.name)}>
         {article.header}
@@ -15,5 +35,7 @@ export default async function Home() {
         </li>
     ))}
   </ul>
-  </>;
+
+  <AppLink href={nextPageUrl}>Next</AppLink>
+  </>);
 }
